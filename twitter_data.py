545fdc9050e79d8600bbb10dc88720a348137550
Sagar Joshi <55200206+sagar0698@users.noreply.github.com>
2020-04-26 04:07:48 -0700

    Create twitter_data.py

---
+++ twitter_data.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 26 03:40:22 2020
+
+@author: spj06
+"""
+
+from twitterscraper import query_tweets
+import datetime as dt 
+import pandas as pd 
+
+
+begin_date = dt.date(2019,4,15)
+end_date = dt.date(2019,4,18)
+
+
+limit = 1000
+lang = 'english'
+
+
+tweets = query_tweets('#wildfire AND #forestfire', begindate = begin_date, enddate = end_date, limit = limit, lang = lang)
+df = pd.DataFrame(t.__dict__ for t in tweets)
+df.to_csv('tweets.csv')
